f191e8d64b66fc8a004940e199e8b9a4f38f6cab
Wagner <wagnerweb2010@gmail.com>
2023-02-04 16:14:31 -0300

    changed key layout

--- Assets/Player/player.gd
+++ Assets/Player/player.gd
@@ -29,13 +29,13 @@ func _process(_delta):
 
 func _movement():
     var direction = Vector2.ZERO
-    if Input.is_action_pressed("ui_up"):
+    if Input.is_action_pressed("move_up"):
         direction += Vector2( 0, -1);
-    if Input.is_action_pressed("ui_down"):
+    if Input.is_action_pressed("move_down"):
         direction += Vector2( 0, +1);
-    if Input.is_action_pressed("ui_left"):
+    if Input.is_action_pressed("move_left"):
         direction += Vector2(-1,  0);
-    if Input.is_action_pressed("ui_right"):
+    if Input.is_action_pressed("move_right"):
         direction += Vector2( 1,  0);
 
     if direction.length() != 0:
