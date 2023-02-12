4f42d6205477b913aa36c862ba3c532b3321e0bf
Wagner <wagnerweb2010@gmail.com>
2023-02-11 22:09:48 -0300

    Fixed a bug in requesting the robot to move

--- Levels/Proto/GUI.gd
+++ Levels/Proto/GUI.gd
@@ -1,6 +1,8 @@
 extends Control
 
 signal robot_move_request(position)
+export(NodePath) var root_node
+onready var scene:Node2D = get_node(root_node)
 
 
 func _ready():
@@ -14,4 +16,4 @@ func _process(_delta):
 func _on_MouseArea_gui_input(event):
     if event is InputEventMouseButton:
         if event.is_action_released("move_request"):
-            emit_signal("robot_move_request", get_global_mouse_position())
+            emit_signal("robot_move_request", scene.get_global_mouse_position())
